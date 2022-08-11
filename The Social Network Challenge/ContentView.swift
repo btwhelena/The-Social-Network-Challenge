d3b4e19a2c00dc48e5010eb0f1deee28775cdb19
Helena <helenaoliveira@alu.ufc.br>
2022-08-11 08:10:27 -0300

    Initial Commit

---
+++ The Social Network Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  The Social Network Challenge
+//
+//  Created by Helena Oliveira on 11/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ The Social Network Challenge/The_Social_Network_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Social_Network_ChallengeApp.swift
+//  The Social Network Challenge
+//
+//  Created by Helena Oliveira on 11/08/22.
+//
+
+import SwiftUI
+
+@main
+struct The_Social_Network_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
